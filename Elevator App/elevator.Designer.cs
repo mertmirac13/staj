a958eb2ff364f444baee14184b42d2302d3bfd98
mertmirac13 <103437979+mertmirac13@users.noreply.github.com>
2023-05-19 17:14:28 +0300

    Add files via upload

---
+++ Elevator App/elevator.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace asansor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.varislabel = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button12 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(88, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kat:1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(88, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Kat:2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(88, 128);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Kat:3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(88, 157);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Kat:4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(88, 186);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Kat:5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(88, 215);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Kat:6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(88, 244);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Kat:7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(88, 273);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Kat:8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(88, 302);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "Kat:9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(88, 331);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Kat:10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(303, 70);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "? Kat";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Lime;
+            this.textBox1.Location = new System.Drawing.Point(197, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Katlar";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(186, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Tahmini Varış Süresi :";
+            // 
+            // varislabel
+            // 
+            this.varislabel.AutoSize = true;
+            this.varislabel.Location = new System.Drawing.Point(300, 128);
+            this.varislabel.Name = "varislabel";
+            this.varislabel.Size = new System.Drawing.Size(0, 13);
+            this.varislabel.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(224, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(47, 20);
+            this.textBox2.TabIndex = 16;
+            // 
+            // button12
+            // 
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(446, 438);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.varislabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Asansör";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label varislabel;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button12;
+    }
+}
+
---
+++ Elevator App/elevator.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace asansor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int gitmekistenilenkat = 0;
+        int asansornerede = 0;
+        int gorevlistesi = 0;
+        int sayac;
+        bool timerbasladimi = false;
+        int aktifgorev = 0;
+        Button[] katlar;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            katlar = new Button[10] { button1, button2, button3, button4, button5, button6, button7, button8, button9, button10 };
+        }
+        Button oncekikat;
+        List<int> firstlist = new List<int>();
+        int gorevbitti = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {            
+            if (gorevlistesi < firstlist.Count)
+            {
+                if (asansornerede < firstlist[gorevlistesi])
+                {
+                    if (oncekikat != null) oncekikat.BackColor = Color.Red;
+                    katlar[asansornerede].BackColor = Color.Orange;
+                    varislabel.Text = (Math.Abs(Convert.ToDouble(gitmekistenilenkat) - Convert.ToDouble(asansornerede))).ToString();
+                    oncekikat = katlar[asansornerede];
+                    asansornerede++;
+                }
+                else if (asansornerede > firstlist[gorevlistesi])
+                {
+                    if (oncekikat != null) oncekikat.BackColor = Color.Red;
+                    katlar[asansornerede].BackColor = Color.Orange;
+                    varislabel.Text = (Math.Abs(Convert.ToDouble(gitmekistenilenkat) - Convert.ToDouble(asansornerede))).ToString();
+                    oncekikat = katlar[asansornerede];
+                    asansornerede--;
+                }
+                else if (asansornerede == firstlist[gorevlistesi])
+                {
+                    if (oncekikat != null) oncekikat.BackColor = Color.Red;
+                    varislabel.Text = (Math.Abs(Convert.ToDouble(gitmekistenilenkat) - Convert.ToDouble(asansornerede))).ToString();
+                    katlar[asansornerede].BackColor = Color.Green;
+                    sayac = 0;
+                    gorevlistesi++;
+                }
+            }           
+                if (sayac == 10 && asansornerede+1 <= 5)
+                {
+                    firstlist.Add(0);
+                    aktifgorev++;
+                }
+                else if (sayac == 10 && asansornerede+1 > 5)
+                {
+                    firstlist.Add(9);
+                    aktifgorev++;
+                }                      
+            sayac++;
+        }
+        bool tekrarbasilmadi = false;
+        private void button11_Click(object sender, EventArgs e)
+        {           
+            if (firstlist.Count != 0)
+            {
+                if (firstlist[firstlist.Count - 1] == Convert.ToInt16(textBox1.Text)-1)
+                {
+                    MessageBox.Show("Zaten Bastınız", "HATA!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    tekrarbasilmadi = true;
+                }
+                else
+                {
+                    tekrarbasilmadi = false;
+                }             
+            }         
+            if (!tekrarbasilmadi)
+            {
+                gitmekistenilenkat = Convert.ToInt16(textBox1.Text) - 1;
+                
+                if (timerbasladimi == false)
+                {
+                    timer1.Start();
+                }
+                firstlist.Add(Convert.ToInt16(textBox1.Text) - 1);
+                aktifgorev++;
+            }           
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                if (Convert.ToInt16(textBox1.Text) > 10)
+                {
+                    MessageBox.Show("10'dan büyük sayı giremezsiniz", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    textBox1.Clear();
+                }
+            }                
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Secenekler secondform = new Secenekler();
+            secondform.Show();
+        }
+    }
+}
